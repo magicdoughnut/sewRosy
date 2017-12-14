74746abf359fbec7d301a4235eb560429a6bb79f
Mark <markgrahamdawson@gmail.com>
2017-12-14 10:15:02 +0000

    Initial styling bootstrap

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('HomePage sewRosy')
+	return render(request, 'home/home.html')
--- sewRosy/settings.py
+++ sewRosy/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    '/home/static/',
+)
+
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
+
