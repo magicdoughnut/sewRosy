9034e3382128457f7e126860cf9fb3cd8bceebe1
Mark <markgrahamdawson@gmail.com>
2017-12-15 09:42:49 +0000

    First draft of complete site

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
+    url(r'^about/$', views.about, name = 'about'),
+    url(r'^gallery/$', views.gallery, name = 'gallery'),
+    url(r'^contact/$', views.contact, name = 'contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	return render(request, 'home/home.html')
+
+def about(request):
+	return render(request, 'home/about.html')
+
+def gallery(request):
+	return render(request, 'home/gallery.html')
+
+def contact(request):
+	return render(request, 'home/contact.html')
